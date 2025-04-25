0583d1c3dc6f3c1ac2ccfa9a643527badca4057e
Nova <50601927+bathtubfulloftoast@users.noreply.github.com>
2025-04-24 21:11:09 -0600

    successful grabbing from lastfm
    now to make it work upon first cache
    fum.

---
+++ functions/grab.js
@@ -0,0 +1,19 @@
+export async function grabimage(file) {
+    let url = `https://lastfm.freetls.fastly.net/i/u/${file}`;
+
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+        console.log(`cover "${file}" failed to grab with status ${response.status} (${response.statusText})`);
+        return "error"; // technology!!!!!! isnt it so wonderfulk....
+        }
+
+        console.log(`grabbed "${file}" from lastfm`);
+        const buffer = await response.arrayBuffer();
+        return Buffer.from(buffer);
+    } catch (error) {
+        console.error({ error: error.message });
+        return null;
+    }
+}
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
+import fs from 'node:fs';
+
 import { convertimage } from './convert.js';
+import { grabimage } from './grab.js';
 
 let size;
 let file;
@@ -18,7 +21,14 @@ file = req.query.file;
 format = req.query.format;
 censored = req.query.censored;
 
+var filematch = file.match(/(?:\/|\\)?([^\/\\]+)\.(\w+)$/);
+
+let filnme = filematch?.[1];
+let filext = filematch?.[2];
+
 format = format.toLowerCase();
+filext = filext.toLowerCase();
+
 
 if (size > 2048) {
 size = 2048;
@@ -34,19 +44,47 @@ censored = 5;
 censored = 0;
 }
 
+
+
 if (/[^0-9]/.test(size)) {
 return res.status(400).json({error:"invalid size"});
+} else if (/[^0-9]/.test(censored)) {
+return res.status(400).json({error:"invalid censorship level"});
+} else if (!filetypes.includes(format)) {
+return res.status(400).json({error:"invalid conversion filetype"});
+} else if (!filetypes.includes(filext)) {
+return res.status(400).json({error:"invalid filetype"});
+} else if (!fs.existsSync(`full/${file}`)) {
+const grabbedimage = await grabimage(file);
+
+if (grabbedimage == "error") {
+return res.status(400).json({error:"invalid album cover"});
+} else {
+
+fs.writeFile(`./full/${file}`, grabbedimage, err => {
+if (err) {
+console.error(err);
+} else {
+console.log(`cached "${file}" successfully`);
+}
+});
+}
 }
 
-if (/[^0-9]/.test(censored)) {
-return res.status(400).json({error:"invalid censorship level"});
-}
 
 size = Math.pow(2, Math.floor(Math.log2(size)));
 
+
 const outputimage = await convertimage(file, size, format, censored);
 res.set('Content-Type', 'image/jpeg');
+res.setHeader('Content-Disposition', `inline; filename="${filnme}.${format}"`);
+
 return res.status(200).send(outputimage);
+
+
+
+
+
 
 //return res.status(200).json({size:size,file:file,format:format,censored:censored});
 }
